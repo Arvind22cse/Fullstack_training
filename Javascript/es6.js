fe3bf9b49712254326eab5169cf763cde8e4307c
ARVIND M <arvindm.22cse@kongu.edu>
2024-12-28 15:25:09 +0530

    javascript

---
+++ Javascript/es6.js
@@ -0,0 +1,49 @@
+function fun(){
+console.log("Fun");
+}
+fun()
+
+fun2=(a,b)=>{
+    return a+b;
+}
+console.log(fun2(1,2));
+
+//destructuring
+var mark=[90,95,99]
+var[m1,m2]=mark
+console.log(m1,m2);
+
+
+//spread
+
+a=["hai","bye","hello","world"]
+b=["arvi","luffy","zoro"]
+
+
+comb=[...a,...b]
+console.log(comb);
+
+//rest
+const[x,y,...z]=a;
+console.log(x);
+console.log(y);
+console.log(z);
+
+
+function rest(...foods){
+
+}
+
+//scope
+//let-block or local scope
+//const,var-global scope 
+a=5
+b=5
+console.log(a,b);
+{
+    let a=10
+    let b=20
+    console.log(a,b);
+}
+console.log(a,b);
+
