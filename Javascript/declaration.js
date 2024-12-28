d1bac647f1cb9cff837969fb6fda55674912d6a3
ARVIND M <arvindm.22cse@kongu.edu>
2024-12-28 11:41:11 +0530

    javascript

---
+++ Javascript/declaration.js
@@ -0,0 +1,7 @@
+a=10
+console.log(a);
+var b="cse"
+console.log(b);
+const c="https://kongu.ac.in";
+console.log(c);
+
---
+++ Javascript/object&operators.js
@@ -0,0 +1,56 @@
+//obj1
+// obj={
+//     "a":1,
+//     "b":"hai"
+// }
+// console.log(obj);
+//obj2
+// obj1={}
+// obj1["name"]="arvind"
+// obj1["age"]=20
+// console.log(obj1.age); 
+// obj2=new Object()
+// console.log(obj2);
+
+
+// a=[1,2,3,[5.5,6.6],"hello",true]
+// console.log(a);
+// for(var b in a){
+//     console.log(b);
+// }
+// for(var c of a){
+//     console.log(c);
+// }
+//Arithmetic op
+// console.log(1+2)
+// console.log(1-2);
+// console.log(1*2);
+// console.log(2/2);
+// console.log(2%2);
+
+//relation operator
+// console.log(1<2)
+// console.log(1>2);
+// console.log(2=="2");
+// console.log(2==="2");
+// console.log(2!=2);
+
+//logical
+console.log(1&&1);
+console.log(1||2);
+console.log(1!=2);
+
+
+
+
+
+//bitwise operator
+console.log(1&2)
+console.log(1|2);
+console.log(2^1);
+console.log(~2);
+console.log(2>>1);
+console.log(2<<3);
+
+//ternary operator
+console.log((1>2)?"true":"false")
