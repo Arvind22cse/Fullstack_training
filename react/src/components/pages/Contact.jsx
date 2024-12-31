bb2b93dda52819f5e450cf6472c973f988a602a8
ARVIND M <arvindm.22cse@kongu.edu>
2024-12-31 09:23:25 +0530

    Day 3 task

--- react/src/components/pages/Contact.jsx
+++ react/src/components/pages/Contact.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import '../../assets/Css/Contact.css'
 export default function Contact() {
   return (
     <div>
       Contact
+
+      <footer className='foot'>
+      <h1>Footer</h1>
+      </footer>
     </div>
   )
 }
