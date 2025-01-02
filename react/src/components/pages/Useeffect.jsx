820761b4820fc08ffbcd6d3aed219fe4f829df14
ARVIND M <arvindm.22cse@kongu.edu>
2025-01-02 11:28:52 +0530

    Day 5

--- react/src/components/pages/Useeffect.jsx
+++ react/src/components/pages/Useeffect.jsx
@@ -84,20 +84,59 @@
 // }
 
 // export default Useffect;
+// www.themealdb.com/api/json/v1/1/categories.php
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+
+// function Useffect() {
+//   var [post, setPost] = useState([]);
+//   const [fetch, setFetch] = useState(false);
+
+//   useEffect(() => {
+//     if (fetch) {
+//       axios
+//         .get("https://jsonplaceholder.typicode.com/posts/")
+//         .then((res) => {
+//           console.log(res.data);
+//           setPost(res.data);
+//         })
+//         .catch((err) => console.log(err));
+//     }
+//   }, [fetch]);
+
+//   const fetchData = () => {
+//     setFetch(true);
+//   };
+
+//   return (
+//     <section>
+//       <h1>Fetching data from Json placeholder API</h1>
+//       <button onClick={fetchData}>Fetch Posts</button>
+//       <h2>Posts are</h2>
+//       <ol>
+//         {post.map((data) => (
+//           <li key={data.id}>{data.title}</li>
+//         ))}
+//       </ol>
+//     </section>
+//   );
+// }
+
+// export default Useffect;
 import { useState, useEffect } from "react";
 import axios from "axios";
 
 function Useffect() {
-  var [post, setPost] = useState([]);
+  const [post, setPost] = useState([]);
   const [fetch, setFetch] = useState(false);
 
   useEffect(() => {
     if (fetch) {
       axios
-        .get("https://jsonplaceholder.typicode.com/posts/")
+        .get("https://www.themealdb.com/api/json/v1/1/categories.php")
         .then((res) => {
           console.log(res.data);
-          setPost(res.data);
+          setPost(res.data.categories); // Update this line
         })
         .catch((err) => console.log(err));
     }
@@ -109,12 +148,15 @@ function Useffect() {
 
   return (
     <section>
-      <h1>Fetching data from Json placeholder API</h1>
+      <h1>Fetching data from JSON Placeholder API</h1>
       <button onClick={fetchData}>Fetch Posts</button>
       <h2>Posts are</h2>
       <ol>
         {post.map((data) => (
-          <li key={data.id}>{data.title}</li>
+          <>
+            <img src={data.strCategoryThumb} alt={data.strCategory} />
+            <li key={data.idCategory}>{data.strCategory}</li>
+          </>
         ))}
       </ol>
     </section>
